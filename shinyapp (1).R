4c38e8d958e25359023a22c95f7cb4b2790da70b
jooaok <120397274+jooaok@users.noreply.github.com>
2022-12-12 15:22:07 +0000

    Add files via upload

---
+++ shinyapp (1).R
@@ -0,0 +1,105 @@
+library(shiny)
+library(ggplot2)
+
+incendios <- read.csv("n_incendios.csv", header = T, sep = ";")
+incendios <- as.data.frame(incendios)
+ardida <- read.csv("area_ardida.csv", header = T, sep = ";")
+ardida$Anos <- as.character(ardida$Anos)
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Fires"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select the random distribution type ----
+      selectInput("region", "Region:", 
+                  choices=colnames(ardida[,-1]))),
+      
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Area ardida por Pais", plotOutput("firePlot")),
+                  tabPanel("Numero incendios por pais", plotOutput("numberPlot")),
+                  tabPanel("Modelo", verbatimTextOutput("summary")),
+                  tabPanel("Area vs N incendios", plotOutput("vsplot"))
+                  
+      )
+      
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram
+server = function(input, output) {
+  
+  d <- reactive({
+    dist <- switch(input$region)
+    
+   
+  })
+  
+  # Fill in the spot we created for a plot
+  output$firePlot <- renderPlot({
+    
+    dist <- input$region
+    
+    # Render a barplot
+    ggplot(ardida, aes(main=input$region, y=ardida[,input$region], x=Anos))+
+      geom_bar(stat = "identity") + 
+      xlab("Anos") +
+      ylab("") + 
+      coord_flip()
+  })
+  
+  output$numberPlot <- renderPlot({
+    
+    dist <- input$region
+    
+    # Render a barplot
+    ggplot(incendios, aes(main=input$region, y=incendios[,input$region], x=Anos))+
+      geom_bar(stat = "identity") + 
+      xlab("Anos") +
+      ylab("")+
+      coord_flip()
+  })
+  
+
+  # Generate a summary of the data ----
+  f <- reactive({
+    as.formula(paste(ardida[,input$region] ~ incendios[, input$region]))
+  })
+
+  Linear_Model <- reactive({
+    lm(f())
+  })
+  
+  output$summary <- renderPrint({
+    dist <- input$region
+    summary(Linear_Model())
+  })
+  
+  
+  
+  output$vsplot <- renderPlot({
+    dist <- input$region
+    
+   plot( incendios[,input$region], ardida[,input$region], xlab="N incendios", ylab = "Area ardida", main=input$region)
+   abline(Linear_Model())
+    
+  })
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
